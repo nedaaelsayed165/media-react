cb6cdd2d6081e9efc8419f86d08dbc7663c67216
nedaaelsayed165 <nedaaelsayed321@gmail.com>
2024-08-02 17:19:40 +0300

    fix: :bug: testing

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import LeftSide from "../LeftSidebar/LeftSide";
 import CardSection from "../Main/CardSection";
-import Navbar from "../Navbar/Navbar";
+// import Navbar from "../Navbar/Navbar";
 import RightSide from "../RightSidebar/RightSide";
 import Main from "../Main/Main";
 
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div className="w-full">
       <div className="fixed top-0 z-10 w-full bg-white">
-        <Navbar></Navbar>
+        {/* <Navbar></Navbar> */}
       </div>
       <div className="flex bg-gray-100">
         <div className="flex-auto w-[20%] fixed top-12">
